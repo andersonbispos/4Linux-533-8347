ee54e3f00ccf53b0fbe467a2bb9ed6f2d0775c12
Anderson Bispo <anderson.bispo@4linux.com.br>
2024-03-14 20:47:33 -0300

    sg alterado

--- 01-AULAS/AULA04/03-sg.tf
+++ 01-AULAS/AULA04/03-sg.tf
@@ -12,6 +12,14 @@ resource "aws_security_group" "sg_default" {
   }
 
   ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "Allow ICMP"
     from_port   = -1
     to_port     = -1
