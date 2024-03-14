d9b82538177e8991be36aa618df6b3c8986c8d14
Anderson Bispo <anderson.bispo@4linux.com.br>
2024-03-14 20:09:16 -0300

    security group

---
+++ 01-AULAS/AULA04/03-sg.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "sg_default" {
+  name        = "vpc_terraform_allow_default"
+  description = "Allow default inbound traffic and all outbound traffic"
+  vpc_id     = "vpc-0702f3e84145d8d8c"
+
+  ingress {
+    description = "Allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow ICMP"
+    from_port        = -1
+    to_port          = -1
+    protocol         = "icmp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
