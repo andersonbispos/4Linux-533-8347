c879162a6bebfb1ee7861bd2ff25be30cb374fce
Anderson Bispo <anderson.bispo@4linux.com.br>
2024-03-15 20:58:13 -0300

    aula05 - part02

---
+++ 01-AULAS/AULA05/parte2/import.tf
@@ -0,0 +1,14 @@
+import {
+  to = aws_subnet.subnet1
+  id = "subnet-0622147032b4c4668"
+}
+
+import {
+  to = aws_subnet.subnet2
+  id = "subnet-098fe599b53234140"
+}
+
+import {
+  to = aws_security_group.sg_instance_default
+  id = "sg-05beb64de35bfa2c2"
+}
---
+++ 01-AULAS/AULA05/parte2/instance.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "web" {
+  ami = "ami-0e0bf53f6def86294"
+
+  availability_zone = "us-east-2b"
+  instance_type     = "t2.micro"
+  subnet_id         = "subnet-0ae37c3e603c653c9"
+
+  tags = {
+    "Name" = "web2"
+  }
+
+  tags_all = {
+    "Environment" = "lab-4linux"
+    "Name"        = "web2"
+    "aula"        = "Aula04"
+    "terraform"   = "yes"
+  }
+
+  vpc_security_group_ids = [
+    "sg-0a284a22650da62cd",
+  ]
+}
