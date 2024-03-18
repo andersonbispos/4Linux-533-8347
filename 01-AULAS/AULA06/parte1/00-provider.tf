683c8dcbe95413df86ce98cd7de863db4b58dca9
Anderson Bispo <anderson.bispo@4linux.com.br>
2024-03-18 19:57:25 -0300

    aula06 backend

---
+++ 01-AULAS/AULA06/parte1/00-provider.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.40.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "trfm-state-bucket-abs"
+    key            = "terraform-state/lab/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "terraform-lock"
+    #access_key = ""
+    #secret_key = ""
+  }
+
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-2"
+
+  default_tags {
+    tags = {
+      Environment = "lab-4linux"
+      terraform   = "yes"
+      aula        = "Aula04"
+    }
+  }
+}
+
+
+
