9beb30b41baa7cae81e3fca4ece5fd487897b6c4
Anderson Bispo <anderson.bispo@4linux.com.br>
2024-03-18 21:53:30 -0300

    aula06-parte02

---
+++ 01-AULAS/AULA06/parte2/02-subnet.tf
@@ -0,0 +1,23 @@
+locals {
+  vpcid = "vpc-07b288829dc3cffa1"
+}
+
+resource "aws_subnet" "vpc_teste_subnet1" {
+  vpc_id            = local.vpcid
+  availability_zone = "us-east-2a"
+  cidr_block        = "192.168.10.0/24"
+
+  tags = {
+    Name = "vpc_teste_subnet1"
+  }
+}
+
+resource "aws_subnet" "vpc_teste_subnet2" {
+  vpc_id            = local.vpcid  
+  availability_zone = "us-east-2a"
+  cidr_block        = "192.168.20.0/24"
+
+  tags = {
+    Name = "vpc_teste_subnet2"
+  }
+}
---
+++ 01-AULAS/AULA06/parte2/03-sg_teste.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "sg_vpc_teste" {
+  name        = "sg_vpc_teste"
+  description = "sg_vpc_teste"
+
+  vpc_id            = local.vpcid
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "sg_vpc_teste"
+  }
+}
