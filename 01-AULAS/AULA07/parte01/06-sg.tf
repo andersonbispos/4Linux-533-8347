b728864f1aac912dd8af51e10fa4d5893c993309
Anderson Bispo <anderson.bispo@4linux.com.br>
2024-03-20 19:30:34 -0300

    aula07 sg

---
+++ 01-AULAS/AULA07/parte01/06-sg.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "sg_default" {
+  name        = format("%s-sg-default", var.vpc_name)
+  description = "Allow default inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.vpc_terraform.id
+
+  tags = {
+    Name = format("%s-sg-default", var.vpc_name)
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow ICMP"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
