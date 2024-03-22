a72da6e1ebf633a2ff954355b2f93cf5f66dd31d
Anderson Bispo <anderson.bispo@4linux.com.br>
2024-03-21 21:06:05 -0300

    subnets

--- 01-AULAS/AULA08/parte2/03-subnets.tf
+++ 01-AULAS/AULA08/parte2/03-subnets.tf
@@ -2,13 +2,13 @@ resource "aws_subnet" "subnet1" {
 
   vpc_id = aws_vpc.tf-vpc-lab.id
 
-  cidr_block = var.subnet1_cidr
+  cidr_block = var.subnets_cidr_list[0]
 
-  availability_zone       = var.subnet1_zone
+  availability_zone       = var.subnets_zones_list[0]
   map_public_ip_on_launch = var.public_ip_enabled
 
   tags = {
-    Name = format("subnet-%s-%s", var.vpc_name, var.subnet1_zone)
+    Name = format("subnet-%s-%s", var.vpc_name, var.subnets_zones_list[0])
   }
 }
 
@@ -21,13 +21,13 @@ resource "aws_subnet" "subnet2" {
 
   vpc_id = aws_vpc.tf-vpc-lab.id
 
-  cidr_block = var.subnet2_cidr
+  cidr_block = var.subnets_cidr_list[1]
 
-  availability_zone       = var.subnet2_zone
+  availability_zone       = var.subnets_zones_list[1]
   map_public_ip_on_launch = var.public_ip_enabled
 
   tags = {
-    Name = format("subnet-%s-%s", var.vpc_name, var.subnet2_zone)
+    Name = format("subnet-%s-%s", var.vpc_name, var.subnets_zones_list[1])
   }
 }
 
