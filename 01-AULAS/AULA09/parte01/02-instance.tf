b406e9b219297eb2cb3ad1cdc37ea19f18ac6697
Anderson Bispo <anderson.bispo@4linux.com.br>
2024-03-22 19:33:04 -0300

    instance

--- 01-AULAS/AULA09/parte01/02-instance.tf
+++ 01-AULAS/AULA09/parte01/02-instance.tf
@@ -1,12 +1,10 @@
 resource "aws_instance" "web" {
 
   ami           = data.aws_ami.ami_amazon_linux.id
-  
+
   instance_type = "t2.micro"
 
-  availability_zone = "us-east-2b"
-
   tags = {
-    Name = "web2"
+    Name = "web-${terraform.workspace}"
   }
 }
