ecc7f85b5d9e3479b00fd86f545afba8933effbd
Anderson Bispo <anderson.bispo@4linux.com.br>
2024-03-25 20:55:20 -0300

    aula 10 + usando modulos

--- 01-AULAS/AULA10/parte1/01-infra.tf
+++ 01-AULAS/AULA10/parte1/01-infra.tf
@@ -2,7 +2,7 @@
 # git tag -a 1.5 -m "modulo com security group"
 
 module "rede" {
-  source = "git::https://github.com/andersonbispos/4Linux-533-8347.git//01-AULAS/AULA09/parte02/modulos/redepublica"
+  source = "git::https://github.com/andersonbispos/4Linux-533-8347.git//01-AULAS/AULA09/parte02/modulos/redepublica?ref=1.0"
 
   vpc_cidr = "192.168.0.0/16"
   region   = "us-east-2"
@@ -14,9 +14,35 @@ resource "aws_instance" "web" {
 
   subnet_id = module.rede.subnet_id
 
-  # vpc_security_group_ids = [aws_security_group.sg_default.id]
-
   tags = {
     Name = "web"
   }
+}
+
+output "web_public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+module "rede_com_sg" {
+  source = "git::https://github.com/andersonbispos/4Linux-533-8347.git//01-AULAS/AULA09/parte02/modulos/redepublica?ref=1.5"
+
+  vpc_cidr = "192.168.0.0/16"
+  region   = "us-east-2"
+}
+
+resource "aws_instance" "web2" {
+  ami           = "ami-0e0bf53f6def86294"
+  instance_type = "t3.micro"
+
+  subnet_id = module.rede_com_sg.subnet_id
+
+  vpc_security_group_ids = [module.rede_com_sg.sg_id]
+
+  tags = {
+    Name = "web2"
+  }
+}
+
+output "web2_public_ip" {
+  value = aws_instance.web2.public_ip
 }
---
+++ 01-AULAS/AULA10/parte1/02-infra2.tf
@@ -0,0 +1,18 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = data.aws_availability_zones.available.names
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
