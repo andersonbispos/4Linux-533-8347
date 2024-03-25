66611dc14c39a6bd432a80117602f6f6c9bde5c0
Anderson Bispo <anderson.bispo@4linux.com.br>
2024-03-25 19:56:19 -0300

    infra aula10

--- 01-AULAS/AULA10/parte1/01-infra.tf
+++ 01-AULAS/AULA10/parte1/01-infra.tf
@@ -1,22 +1,22 @@
 module "rede" {
-  source = "./modulos/redepublica"
+  source = "git::https://github.com/andersonbispos/4Linux-533-8347.git//01-AULAS/AULA09/parte02/modulos/redepublica"
 
   vpc_cidr = "192.168.0.0/16"
   region   = "us-east-2"
 }
 
-# resource "aws_instance" "web" {
-#   ami           = "ami-0e0bf53f6def86294"
-#   instance_type = "t3.micro"
+resource "aws_instance" "web" {
+  ami           = "ami-0e0bf53f6def86294"
+  instance_type = "t3.micro"
 
-#   subnet_id = module.rede.subnet_id
+  subnet_id = module.rede.subnet_id
 
-#   vpc_security_group_ids = [aws_security_group.sg_default.id]
+  # vpc_security_group_ids = [aws_security_group.sg_default.id]
 
-#   tags = {
-#     Name = "web"
-#   }
-# }
+  tags = {
+    Name = "web"
+  }
+}
 
 # output "web_public_ip" {
 #   value = aws_instance.web.public_ip
