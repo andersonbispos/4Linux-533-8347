54b0846954b6b147f20bf9f957f34271685b817e
Anderson Bispo <anderson.bispo@4linux.com.br>
2024-03-25 19:07:02 -0300

    aula10-modulos

--- 01-AULAS/AULA09/parte02/01-infra.tf
+++ 01-AULAS/AULA09/parte02/01-infra.tf
@@ -4,3 +4,17 @@ module "rede" {
   vpc_cidr = "192.168.0.0/16"
   region   = "us-east-2"
 }
+
+resource "aws_instance" "web" {
+  ami           = "ami-0e0bf53f6def86294"
+  instance_type = "t3.micro"
+
+  subnet_id = module.rede.aws_subnet.subnet_public.id
+
+  # vpc_security_group_ids = ["sg-079711cb02c0b71b2"]
+
+  tags = {
+    Name = "web"
+  }
+}
+
---
+++ 01-AULAS/AULA09/parte02/modulos/redepublica/outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_id" {
+    description = "vpc_id"
+    value = aws_vpc.vpc_4linux.id
+}
+
+output "subnet_id" {
+    description = "subnet_id"
+    value = aws_subnet.subnet_public.id
+}
+
+output "public_route_table_id" {
+    description = "public_route_table_id"
+    value = aws_route_table.rt_public.id  
+}
