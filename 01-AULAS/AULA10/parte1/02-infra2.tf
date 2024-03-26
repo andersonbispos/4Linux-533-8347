15add343aec8f018b7eca6a040975039111aac91
Anderson Bispo <anderson.bispo@4linux.com.br>
2024-03-25 21:04:59 -0300

    infra

--- 01-AULAS/AULA10/parte1/02-infra2.tf
+++ 01-AULAS/AULA10/parte1/02-infra2.tf
@@ -9,7 +9,9 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = data.aws_availability_zones.available.names
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
 
   tags = {
     Terraform = "true"
